652ed33b4404a203f0496bc8f4dd3e9e80e7591e
joshtom <olajidejoshua4real@gmail.com>
2019-11-06 17:37:29 +0100

    Added source and destination folder to pipe the images

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
+
+const imageSquash = () => {
+    return gulp.src("./img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./minifies/images/"));
+    
+}
